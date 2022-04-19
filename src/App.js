2e411e8162d2b0e71527f110bd998fd89d172b2c
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-04-19 16:32:07 -0600

    Fixed Login Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import './App.css';
+import './login.css';
 import Search from "./Search";
 import Login from "./components/Login";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import EmployeeButton from "./components/EmployeeButton";
 function App(){
   return(
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,43 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../login.css"
 
 function Login(){
     let navigate = useNavigate();
     return(
-        <div>
+        <div className="container-login">
             <header style={{textAlign: 'center'}}>Employee Login Page!</header>
             
-            <form>
-                <label>
+            <ul>
+                <li>
+                <label   textAlign = 'center'>
                     Username:
-                     <input type="text" name="name" />
+                     <input type="text" name="name"/>
                 </label>
                 <input type="submit" value="Submit" />
-                <label>
+                </li>
+
+                
+                 <li><label>
                     Password:
                      <input type="text" name="name" />
                 </label>
-                <input type="submit" value="Submit" />
-                <label>
-                    RestaurantID:
+                <input type="submit" value="Submit" /></li>
+
+                <li><label>
+                    Restaurant ID:
                      <input type="text" name="name" />
                 </label>
-                <input type="submit" value="Submit" />
-            </form>
-            <ul>
-                <li style={{textAlign: 'center'}}><button > Login </button></li>
-            </ul>  
+                <input type="submit" value="Submit" /></li>
+                <li style={{textAlign: 'center'}}><button>Login</button></li> 
+
+
+                <li style={{textAlign: 'left'}}><button onClick={() =>(navigate("/"))}>Back </button></li>
+                 
+                 
+            </ul>
+         
+            
         </div>
     )
 }
