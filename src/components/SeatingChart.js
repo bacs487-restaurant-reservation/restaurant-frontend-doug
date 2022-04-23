b30758cc19f40aae30b19c8e0c3cab4c69f7140c
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-04-22 19:21:12 -0600

    Fixed Seating

--- src/components/SeatingChart.js
+++ src/components/SeatingChart.js
@@ -3,16 +3,13 @@ import { useNavigate } from "react-router-dom";
 import "../seatingchart.css"
 import Popup from "./Popup";
 import { useState } from "react";
-import Search from "../Search";
+
+
 
 function SeatingChart(){
     let navigate = useNavigate();
 
     const[buttonPopup, setButtonPopup] = useState(false)
-
-    const codeGenerator = () =>{
-        window.alert()
-    }
 
     let number = Math.floor(100000 + Math.random() * 900000);
     return(
@@ -21,7 +18,7 @@ function SeatingChart(){
         <div class="container-restaurant">
             <h1 style={{textAlign: 'center' }} ></h1>
             <ul>
-                <div class = "square" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}> <li style={{textAlign: 'center'}}>Table 1</li></div>
+                <div class = "circle" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}> <li style={{textAlign: 'center'}}>Table 1</li></div>
                 <li  table1 style={{textAlign: 'left'}}><button onClick={() =>setButtonPopup(true)}>SELECT</button></li> 
             </ul>
             <ul>
@@ -38,12 +35,11 @@ function SeatingChart(){
             </ul>
             <ul>
             <li><button onClick={() =>(navigate("/"))}>Back </button></li>
- 
             </ul>
             <Popup trigger = {buttonPopup} setTrigger={setButtonPopup}>
-                <p> Your confirmation code to present to the host is:  {number}</p>
-                
+                <p> Your confirmation code to present to the host is:  {number}</p>   
             </Popup>
+            
         </div>
      
      );
